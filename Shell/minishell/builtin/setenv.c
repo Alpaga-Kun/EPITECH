c87b95b42192d142afd3176f5f936b3580a045d3
Alpaga-Kun <alpaga@pc-8.home>
2022-07-12 09:42:01 +0400

    Bug fixed: if the first character is not an alpha for SETENV function

--- Shell/minishell/builtin/setenv.c
+++ Shell/minishell/builtin/setenv.c
@@ -6,19 +6,11 @@
 /*   By: Alpaga-Kun <teambodzen20@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/11 20:31:19 by Alpaga-Kun        #+#    #+#             */
-/*   Updated: 2022/07/12 09:38:26 by Alpaga-Kun       ###   ########.fr       */
+/*   Updated: 2022/07/12 09:41:06 by Alpaga-Kun       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "prototypes.h"
-
-static bool errorSetenv(data_shell *infoShell)
-{
-    for (int i = 0; infoShell->commands[1][i] != '\0'; i++)
-        if (isalpha(infoShell->commands[1][i]))
-            return (true);
-    return (false);
-}
 
 int setenvSystem(data_shell *infoShell)
 {
@@ -28,7 +20,7 @@ int setenvSystem(data_shell *infoShell)
 
     if (!infoShell->commands[1])
         return (-1);
-    if (errorSetenv(infoShell) == false)
+    if (!isalpha(infoShell->commands[1][0]))
         return (setquitMessage(infoShell->commands[0], "Variable name must begin with a letter.", 1));
     if (!(tabs = malloc(sizeof(char **) * (sizeTab(infoShell->myenv) + 2))))
         quitMessage("malloc()", 84);
